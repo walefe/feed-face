a453c4ac8c77910534ba9bb3b0c416621511cbeb
Walefe <walefedev@gmail.com>
2019-09-26 15:53:27 -0300

    Add avatar to header.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 
 import "./styles.css";
 
+import avatar from "../../assets/avatar.svg";
+
 class Header extends Component {
   render() {
     return (
       <header>
         <h1>facebook</h1>
-        <a href="">
-          <h1>Meu perfil</h1>
-        </a>
+        <div className="profile">
+          <a href="">
+            <h1>Meu perfil</h1>
+          </a>
+          <img src={avatar} alt="Avatar" />
+        </div>
       </header>
     );
   }
